fbd57b6bb42af8f1d2ce912ca6f32b6067dc02df
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-28 18:36:24 -0300

    feat: Gateway criado e rota User configurado

---
+++ vinhoJa-Backend/gateway/src/main/java/io/gitHub/AugustoMello09/gateway/GatewayApplication.java
@@ -0,0 +1,28 @@
+package io.gitHub.AugustoMello09.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/users/**").uri("lb://msUsers"))
+				.build();
+	}
+
+}

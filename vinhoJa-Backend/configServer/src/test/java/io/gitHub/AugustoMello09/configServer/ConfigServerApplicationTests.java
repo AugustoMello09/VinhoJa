cdd2bcf548d6819f4f365f6d3c1283f8f8e280b7
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-28 15:27:52 -0300

    feat: Config Server pronto

---
+++ vinhoJa-Backend/configServer/src/test/java/io/gitHub/AugustoMello09/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

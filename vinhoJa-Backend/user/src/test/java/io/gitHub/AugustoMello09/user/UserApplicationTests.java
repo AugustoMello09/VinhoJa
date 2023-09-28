8778f980a8365d06816c3a9da37d5d1d9fb4d976
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-28 09:35:44 -0300

    feat: msUser criado e registrado no eurekaServe

---
+++ vinhoJa-Backend/user/src/test/java/io/gitHub/AugustoMello09/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

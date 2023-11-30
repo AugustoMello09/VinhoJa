5c51f77c6d87bbe5f6ab77e8ccaac4a2d0c75a13
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-11-30 13:09:45 -0300

    feat: Steps para usuario

--- vinhoJa-Frontend/src/app/app.module.ts
+++ vinhoJa-Frontend/src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
 import { ButtonModule } from 'primeng/button';
+import { StepsModule } from 'primeng/steps';
+
 import { HeaderComponent } from './components/template/header/header.component';
-import { UsuarioComponent } from './components/usuario/usuario.component'; 
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { StepsComponent } from './components/template/steps/steps.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    StepsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AccordionModule,
-    ButtonModule
+    ButtonModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StepsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ vinhoJa-Frontend/src/app/components/template/steps/steps.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-steps',
+  templateUrl: './steps.component.html',
+  styleUrls: ['./steps.component.css']
+})
+export class StepsComponent implements OnInit {
+
+  items: MenuItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+          label: 'Dados Pessoais',
+          routerLink: ''
+      },];
+  }
+
+}

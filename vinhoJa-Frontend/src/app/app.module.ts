8ce15b0a41a8a91ead4779028616c0c3a43a61e2
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-11-30 12:48:05 -0300

    feat: PrimeNG e Tailwindcss

--- vinhoJa-Frontend/src/app/app.module.ts
+++ vinhoJa-Frontend/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
+import { ButtonModule } from 'primeng/button'; 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
